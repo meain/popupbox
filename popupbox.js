376540131d96273fdfe13ae355f164a4720e38ec
Abin Simon <abinsimon10@gmail.com>
2016-06-10 18:48:14 +0530

    About functional

--- popupbox.js
+++ popupbox.js
@@ -1,4 +1,29 @@
-$('#button').click(function(){
-    // alert('click');
-    $('#popupbox').css('display', 'block');
+$('#button').click(function(e){
+    e.stopPropagation();
+    // Get the location of the parent so as to positon the popup
+    var element = $('#button');
+    var child = $('#popupboxcontent');
+    var offsets = element.offset();
+    var etop = offsets.top;
+    var eleft = offsets.left;
+    var eheight = element.outerHeight();
+    var ewidth = element.outerWidth();
+    var cwidth = child.outerWidth();
+    var cheight = child.outerHeight();
+    var posy = etop + eheight;
+    var posx = eleft + ewidth/2 - cwidth/2;
+    if(posx < 5){
+        posx = 5;
+    }
+    console.log('posx : ' + posx + ' posy : ' + eheight);
+    $('#popupbox').css('margin-top', posy + 'px');
+    $('#popupbox').css('margin-left', posx + 'px');
+    $('#popupbox').css('display', 'inline');
+});
+$('html').click(function(){
+    $('#popupbox').hide();
+});
+$('#popupboxcontent').click(function(e){
+    e.stopPropagation();
+    $(this).show();
 });
